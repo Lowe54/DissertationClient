e10f76ce23f98f648be360e0ba6a37626b7904e7
James Lowe <jameslowe241@gmail.com>
2020-09-17 12:15:23 +0100

    Add project files.

---
+++ DissertationClient/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace DissertationClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_Menu_Login = new System.Windows.Forms.Button();
+            this.button_Menu_CreateAccount = new System.Windows.Forms.Button();
+            this.button_Menu_View_Images = new System.Windows.Forms.Button();
+            this.button_Upload_Image = new System.Windows.Forms.Button();
+            this.groupBox_CreateAccount = new System.Windows.Forms.GroupBox();
+            this.button_Login = new System.Windows.Forms.Button();
+            this.button_CA_Create = new System.Windows.Forms.Button();
+            this.textBox_Password = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_UserName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.webControl = new Awesomium.Windows.Forms.WebControl(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.backgroundColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox_CreateAccount.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_Menu_Login
+            // 
+            this.button_Menu_Login.Location = new System.Drawing.Point(864, 23);
+            this.button_Menu_Login.Name = "button_Menu_Login";
+            this.button_Menu_Login.Size = new System.Drawing.Size(125, 53);
+            this.button_Menu_Login.TabIndex = 0;
+            this.button_Menu_Login.Text = "Login/Logout";
+            this.button_Menu_Login.UseVisualStyleBackColor = true;
+            this.button_Menu_Login.Click += new System.EventHandler(this.button_Menu_Login_Click);
+            // 
+            // button_Menu_CreateAccount
+            // 
+            this.button_Menu_CreateAccount.Location = new System.Drawing.Point(864, 82);
+            this.button_Menu_CreateAccount.Name = "button_Menu_CreateAccount";
+            this.button_Menu_CreateAccount.Size = new System.Drawing.Size(125, 60);
+            this.button_Menu_CreateAccount.TabIndex = 1;
+            this.button_Menu_CreateAccount.Text = "Create Account";
+            this.button_Menu_CreateAccount.UseVisualStyleBackColor = true;
+            this.button_Menu_CreateAccount.Click += new System.EventHandler(this.button_Menu_CreateAccount_Click);
+            // 
+            // button_Menu_View_Images
+            // 
+            this.button_Menu_View_Images.Location = new System.Drawing.Point(864, 150);
+            this.button_Menu_View_Images.Name = "button_Menu_View_Images";
+            this.button_Menu_View_Images.Size = new System.Drawing.Size(125, 58);
+            this.button_Menu_View_Images.TabIndex = 2;
+            this.button_Menu_View_Images.Text = "View My Images";
+            this.button_Menu_View_Images.UseVisualStyleBackColor = true;
+            this.button_Menu_View_Images.Click += new System.EventHandler(this.button_Menu_View_Images_Click);
+            // 
+            // button_Upload_Image
+            // 
+            this.button_Upload_Image.Location = new System.Drawing.Point(864, 214);
+            this.button_Upload_Image.Name = "button_Upload_Image";
+            this.button_Upload_Image.Size = new System.Drawing.Size(125, 61);
+            this.button_Upload_Image.TabIndex = 3;
+            this.button_Upload_Image.Text = "Upload Image";
+            this.button_Upload_Image.UseVisualStyleBackColor = true;
+            this.button_Upload_Image.Click += new System.EventHandler(this.button_Upload_Image_Click);
+            // 
+            // groupBox_CreateAccount
+            // 
+            this.groupBox_CreateAccount.Controls.Add(this.button_Login);
+            this.groupBox_CreateAccount.Controls.Add(this.button_CA_Create);
+            this.groupBox_CreateAccount.Controls.Add(this.textBox_Password);
+            this.groupBox_CreateAccount.Controls.Add(this.label2);
+            this.groupBox_CreateAccount.Controls.Add(this.textBox_UserName);
+            this.groupBox_CreateAccount.Controls.Add(this.label1);
+            this.groupBox_CreateAccount.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.groupBox_CreateAccount.Location = new System.Drawing.Point(13, 15);
+            this.groupBox_CreateAccount.Margin = new System.Windows.Forms.Padding(0);
+            this.groupBox_CreateAccount.Name = "groupBox_CreateAccount";
+            this.groupBox_CreateAccount.Size = new System.Drawing.Size(823, 416);
+            this.groupBox_CreateAccount.TabIndex = 4;
+            this.groupBox_CreateAccount.TabStop = false;
+            // 
+            // button_Login
+            // 
+            this.button_Login.Location = new System.Drawing.Point(574, 159);
+            this.button_Login.Name = "button_Login";
+            this.button_Login.Size = new System.Drawing.Size(110, 47);
+            this.button_Login.TabIndex = 5;
+            this.button_Login.Text = "Login";
+            this.button_Login.UseVisualStyleBackColor = true;
+            this.button_Login.Click += new System.EventHandler(this.button_Login_Click);
+            // 
+            // button_CA_Create
+            // 
+            this.button_CA_Create.Location = new System.Drawing.Point(574, 159);
+            this.button_CA_Create.Name = "button_CA_Create";
+            this.button_CA_Create.Size = new System.Drawing.Size(110, 47);
+            this.button_CA_Create.TabIndex = 4;
+            this.button_CA_Create.Text = "Create Account";
+            this.button_CA_Create.UseVisualStyleBackColor = true;
+            this.button_CA_Create.Click += new System.EventHandler(this.button_CA_Create_Click);
+            // 
+            // textBox_Password
+            // 
+            this.textBox_Password.Location = new System.Drawing.Point(121, 91);
+            this.textBox_Password.Name = "textBox_Password";
+            this.textBox_Password.PasswordChar = '*';
+            this.textBox_Password.Size = new System.Drawing.Size(525, 20);
+            this.textBox_Password.TabIndex = 3;
+            this.textBox_Password.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // textBox_UserName
+            // 
+            this.textBox_UserName.Location = new System.Drawing.Point(121, 50);
+            this.textBox_UserName.Name = "textBox_UserName";
+            this.textBox_UserName.Size = new System.Drawing.Size(525, 20);
+            this.textBox_UserName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UserName: ";
+            // 
+            // webControl
+            // 
+            this.webControl.Location = new System.Drawing.Point(13, 28);
+            this.webControl.Size = new System.Drawing.Size(824, 402);
+            this.webControl.Source = new System.Uri("http://localhost:8080/DissertationBackend/Welcome.php", System.UriKind.Absolute);
+            this.webControl.TabIndex = 5;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1001, 25);
+            this.toolStrip1.TabIndex = 6;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backgroundColorToolStripMenuItem,
+            this.fontToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(29, 22);
+            this.toolStripDropDownButton1.Text = "Options";
+            // 
+            // backgroundColorToolStripMenuItem
+            // 
+            this.backgroundColorToolStripMenuItem.Name = "backgroundColorToolStripMenuItem";
+            this.backgroundColorToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.backgroundColorToolStripMenuItem.Text = "Background Color";
+            this.backgroundColorToolStripMenuItem.Click += new System.EventHandler(this.backgroundColorToolStripMenuItem_Click);
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.fontToolStripMenuItem.Text = "Font";
+            this.fontToolStripMenuItem.Click += new System.EventHandler(this.fontToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1001, 440);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.webControl);
+            this.Controls.Add(this.groupBox_CreateAccount);
+            this.Controls.Add(this.button_Upload_Image);
+            this.Controls.Add(this.button_Menu_View_Images);
+            this.Controls.Add(this.button_Menu_CreateAccount);
+            this.Controls.Add(this.button_Menu_Login);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Image Storage Client";
+            this.groupBox_CreateAccount.ResumeLayout(false);
+            this.groupBox_CreateAccount.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Menu_Login;
+        private System.Windows.Forms.Button button_Menu_CreateAccount;
+        private System.Windows.Forms.Button button_Menu_View_Images;
+        private System.Windows.Forms.Button button_Upload_Image;
+        private System.Windows.Forms.GroupBox groupBox_CreateAccount;
+        private System.Windows.Forms.Button button_Login;
+        private System.Windows.Forms.Button button_CA_Create;
+        private System.Windows.Forms.TextBox textBox_Password;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_UserName;
+        private System.Windows.Forms.Label label1;
+        private Awesomium.Windows.Forms.WebControl webControl;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem backgroundColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+    }
+}
+
---
+++ DissertationClient/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Web;
+
+namespace DissertationClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Menu_Login_Click(object sender, EventArgs e)
+        {
+
+            String ifTarget1 = "http://localhost:8080/DissertationBackend/sys_Image/Overview.php";
+            String ifTarget2 = "http://localhost:8080/DissertationBackend/sys_Image/ImageUpload.php";
+            Uri urlTarget1 = new Uri(ifTarget1);
+            Uri urlTarget2 = new Uri(ifTarget2);
+            if (webControl.Source == urlTarget1 || webControl.Source == urlTarget2)
+            {
+
+                Uri LogoutTarget = new Uri("http://localhost:8080/DissertationBackend/Sys_Login/login.php");
+                groupBox_CreateAccount.Visible = false;
+                webControl.Visible = true;
+                webControl.Source = LogoutTarget;
+
+            }
+            else
+            {
+                button_CA_Create.Visible = false;
+                groupBox_CreateAccount.Visible = true;
+                button_Login.Visible = true;
+                webControl.Visible = false;
+            }
+        }
+
+        private void button_Menu_CreateAccount_Click(object sender, EventArgs e)
+        {
+            button_CA_Create.Visible = true;
+            groupBox_CreateAccount.Visible = true;
+            button_Login.Visible = false;
+            webControl.Visible = false;
+        }
+
+        private void button_CA_Create_Click(object sender, EventArgs e)
+        {
+            String User = textBox_UserName.Text;
+            String Pass = textBox_Password.Text;
+            String P = "";
+            String i = "";
+            String k = "";
+
+            Console.Write("Calling Encrypt Function" + "\r\n");
+            GenerateKeyIV(out k, out i);
+            Encrypt(Pass, out P, k, i);
+           
+            
+           // Encrypt(Pass,out P,k,i);
+
+
+            Console.WriteLine("Pass is" + Pass + "\r\n");
+            Console.WriteLine("P is" + P + "\r\n");
+            Console.WriteLine("Key is" + k);
+            Console.WriteLine("IV is" + i);
+
+
+            // Deals with the uri and visibility settings
+            String username = "UserName=" + User;
+            String PassWord = "&Password=" + Pass;
+            String INV = "&IV=" + i;
+            String KY = "&K=" + k;
+
+            String target = "http://localhost:8080/DissertationBackend/Sys_Register/RegisterCheck.php?" + username + PassWord + INV + KY;
+            Uri T = new Uri(target);
+            groupBox_CreateAccount.Visible = false;
+            webControl.Visible = true;
+            webControl.Source = T;
+
+
+
+
+        }
+        static String Encrypt(String Pass,out string P, string k, string i)
+        {
+            /*
+             * function static void Encrypt
+             * --------------------------------------------------------
+             * PURPOSE: Encrypt the Password Entered in the Text Box
+             * --------------------------------------------------------
+             * PASSED VARIABLES:
+             * 1) String Pass = unencrypted Password
+             * 2) out String P = Encrypted Password
+             * 
+             * ---------------------------------------------------------
+             */
+          
+           
+             var rj = new RijndaelManaged()
+            {
+                Padding = PaddingMode.PKCS7,
+                Mode = CipherMode.CBC,
+                KeySize = 256,
+                BlockSize = 256,
+            };
+                      
+            
+           
+            
+            var sToEncrypt = Pass;
+
+           
+
+            var key = Convert.FromBase64String(k);
+            var IV = Convert.FromBase64String(i);
+
+           
+            var encryptor = rj.CreateEncryptor(key, IV);
+
+            var msEncrypt = new MemoryStream();
+            var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
+
+            var toEncrypt = Encoding.ASCII.GetBytes(sToEncrypt);
+
+            csEncrypt.Write(toEncrypt, 0, toEncrypt.Length);
+            csEncrypt.FlushFinalBlock();
+
+            var p = msEncrypt.ToArray();
+            P = Convert.ToBase64String(p);
+            return P;
+
+
+        }
+
+        public static void GenerateKeyIV(out string key, out string IV)
+        {
+            var rj = new RijndaelManaged()
+            {
+                Padding = PaddingMode.PKCS7,
+                Mode = CipherMode.CBC,
+                KeySize = 256,
+                BlockSize = 256,
+            };
+            rj.GenerateKey();
+            rj.GenerateIV();
+
+            key = Convert.ToBase64String(rj.Key);
+            IV = Convert.ToBase64String(rj.IV);
+        }
+
+
+        private void button_Login_Click(object sender, EventArgs e)
+        {
+               
+                       
+            String User = textBox_UserName.Text;
+            String Pass = textBox_Password.Text;
+            String P = "";
+            String i = "";
+            String k = "";
+
+
+            Console.WriteLine("Calling Encrypt Function" + "\r\n");
+            GenerateKeyIV(out k, out i);
+            Encrypt(Pass, out P, k, i);
+
+
+            Console.WriteLine("Pass is" + Pass + "\r\n");
+            Console.WriteLine("P is" + P + "\r\n");
+            Console.WriteLine("i = " + i);
+            Console.WriteLine("k = " + k);
+
+            // Deals with the uri and visibility settings
+            String username = "UserName=" + User;
+            String PassWord = "&Password=" + P;
+            String INV = "&IV=" + i;
+            String KY = "&K=" + k;
+
+            String target = "http://localhost:8080/DissertationBackend/Sys_Login/logintosite.php?" + username + PassWord + INV + KY;
+            Uri T = new Uri(target);
+            groupBox_CreateAccount.Visible = false;
+            webControl.Visible = true;
+            webControl.Source = T;
+
+            
+        }
+
+        private void button_Upload_Image_Click(object sender, EventArgs e)
+        {
+            String Tar = "http://localhost:8080/DissertationBackend/sys_Image/ImageUpload.php";
+            Uri Im = new Uri(Tar);
+            webControl.Visible = true;
+            webControl.Source = Im;
+        
+        }
+
+        private void button_Menu_View_Images_Click(object sender, EventArgs e)
+        {
+            String Tar = "http://localhost:8080/DissertationBackend/sys_Image/Overview.php";
+            Uri Im = new Uri(Tar);
+            webControl.Visible = true;
+            webControl.Source = Im;
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog1 = new FontDialog();
+            fontDialog1.ShowColor = true;
+
+            
+            fontDialog1.Font = button_Login.Font;
+            fontDialog1.Color = button_Login.ForeColor;
+
+            if (fontDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                button_Login.Font = fontDialog1.Font;
+                button_Login.ForeColor = fontDialog1.Color;
+
+                button_CA_Create.Font = fontDialog1.Font;
+                button_CA_Create.ForeColor = fontDialog1.Color;
+
+                button_Menu_Login.Font = fontDialog1.Font;
+                button_Menu_Login.ForeColor = fontDialog1.Color;
+
+                button_Menu_CreateAccount.Font = fontDialog1.Font;
+                button_Menu_CreateAccount.ForeColor = fontDialog1.Color;
+
+                button_Menu_View_Images.Font = fontDialog1.Font;
+                button_Menu_View_Images.ForeColor = fontDialog1.Color;
+
+                button_Upload_Image.Font = fontDialog1.Font;
+                button_Upload_Image.ForeColor = fontDialog1.Color;
+
+
+                label1.Font = fontDialog1.Font;
+                label1.ForeColor = fontDialog1.Color;
+
+                label2.Font = fontDialog1.Font;
+                label2.ForeColor = fontDialog1.Color;
+                
+
+
+            }
+        }
+
+        private void backgroundColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog1 = new ColorDialog();
+            DialogResult result = colorDialog1.ShowDialog();
+            // See if user pressed ok.
+            if (result == DialogResult.OK)
+            {
+                // Set form background to the selected color.
+                this.BackColor = colorDialog1.Color;
+                
+            }
+        }
+    }
+}
